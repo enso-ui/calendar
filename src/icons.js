9cdc57f27db78cb20135fb36a3d388f0c2f1fba8
mishu <mishu@xtelecom.ro>
2026-04-28 18:12:29 +0300

    Register package icons

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCalendarDays);
+library.add(faBell, faCalendarDays);
